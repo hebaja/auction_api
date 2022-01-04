0dd53217e5b66915d3b0f59fa2482483e425c630
hebaja <hebaja@yahoo.com.br>
2022-01-04 20:06:08 -0300

    fixing

--- src/main/java/com/hebaja/auction/config/FirebaseConfig.java
+++ src/main/java/com/hebaja/auction/config/FirebaseConfig.java
@@ -1,8 +1,12 @@
 package com.hebaja.auction.config;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.io.IOUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -10,17 +14,45 @@ import com.google.firebase.FirebaseOptions;
 public class FirebaseConfig {
 	
 	public void configure() throws IOException {
-		FileInputStream credentials = new FileInputStream("/app/auction-5271a-firebase-adminsdk-4r35f-eddbeffed8.json");
-		GoogleCredentials googleCredentials = GoogleCredentials.fromStream(credentials);
+		
+		InputStream firebaseCredentialStream = null;
+		try {
+			firebaseCredentialStream = createFirebaseCredential();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		FirebaseOptions options = FirebaseOptions
 				.builder()
-				.setCredentials(googleCredentials)
+				.setCredentials(GoogleCredentials.fromStream(firebaseCredentialStream))
 				.build();
 		
 		System.out.println(this.getClass().toString() + " starting firebase");
 		
 		FirebaseApp.initializeApp(options);
 		System.out.println("[FirebaseConfig] Starting firebase with app name: " + FirebaseApp.DEFAULT_APP_NAME );
+	}
+	
+	private InputStream createFirebaseCredential() throws Exception {
+	    String privateKey = System.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n");
+
+	    FirebaseCredential firebaseCredential = new FirebaseCredential();
+	    firebaseCredential.setType(System.getenv("FIREBASE_TYPE"));
+	    firebaseCredential.setProject_id(System.getenv("FIREBASE_PROJECT_ID"));
+	    firebaseCredential.setPrivate_key_id(System.getenv("FIREBASE_PRIVATE_KEY_ID"));
+	    firebaseCredential.setPrivate_key(privateKey);
+	    firebaseCredential.setClient_email(System.getenv("FIREBASE_CLIENT_EMAIL"));
+	    firebaseCredential.setClient_id(System.getenv("FIREBASE_CLIENT_ID"));
+	    firebaseCredential.setAuth_uri(System.getenv("FIREBASE_AUTH_URI"));
+	    firebaseCredential.setToken_uri(System.getenv("FIREBASE_TOKEN_URI"));
+	    firebaseCredential.setAuth_provider_x509_cert_url(System.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"));
+	    firebaseCredential.setClient_x509_cert_url(System.getenv("FIREBASE_CLIENT_X509_CERT_URL"));
+	    //serialization of the object to json string
+	    ObjectMapper mapper = new ObjectMapper();
+	    String jsonString = mapper.writeValueAsString(firebaseCredential);
+
+	    //convert jsonString string to InputStream using Apache Commons
+	    return IOUtils.toInputStream(jsonString, StandardCharsets.UTF_8);
 	}
 
 }
